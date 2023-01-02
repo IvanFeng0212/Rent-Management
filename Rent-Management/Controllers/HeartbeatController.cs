93d26839095f92e581ff7f71e1c96bd2c69ab529
ivanfeng <ivanfeng0212@gmail.com>
2023-01-02 10:01:09 +0800

    Add heartbeat api

---
+++ Rent-Management/Controllers/HeartbeatController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rent_Management.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HeartbeatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Heartbeat()
+        {
+            return Ok();
+        }
+    }
+}
